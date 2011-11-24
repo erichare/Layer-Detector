ebc10e7dce0310fa302110c1998343356fd1dc04
Eric Hare <Raftysworld@gmail.com>
2011-11-24 12:52:39 -0800

    Include initial python layer detector

---
+++ layerdetector.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+import sys
+import cv2.cv as cv
+
+# some definitions
+win_name = "Edge"
+
+# definition of some colors
+_red =  (0, 0, 255, 0);
+_green =  (0, 255, 0, 0);
+_white = cv.RealScalar (255)
+_black = cv.RealScalar (0)
+
+im = cv.LoadImage(sys.argv[1], cv.CV_LOAD_IMAGE_COLOR)
+
+# create the output im
+col_edge = cv.CreateImage((im.width, im.height), 8, 3)
+
+# convert to grayscale
+gray = cv.CreateImage((im.width, im.height), 8, 1)
+edge = cv.CreateImage((im.width, im.height), 8, 1)
+cv.CvtColor(im, gray, cv.CV_BGR2GRAY)
+
+# create the window
+cv.NamedWindow(win_name, cv.CV_WINDOW_AUTOSIZE)
+
+# show the im
+cv.Smooth(gray, edge, cv.CV_BLUR, 3, 3, 0)
+cv.Not(gray, edge)
+
+# run the edge dector on gray scale
+threshold = 100
+cv.Canny(gray, edge, threshold, threshold * 3, 3)
+
+# copy edge points
+cv.Copy(im, col_edge, edge)
+
+# show the im
+cv.ShowImage(win_name, col_edge)
+
+cv.WaitKey(0)
